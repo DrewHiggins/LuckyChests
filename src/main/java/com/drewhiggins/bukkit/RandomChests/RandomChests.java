6ef1a0e75d64a5ce9e851a88e78ec6bfef438069
DrewHiggins <me@drewhiggins.com>
2014-07-22 15:48:55 -0400

    added extrema for generation method

--- src/main/java/com/drewhiggins/bukkit/RandomChests/RandomChests.java
+++ src/main/java/com/drewhiggins/bukkit/RandomChests/RandomChests.java
@@ -1,6 +1,5 @@
 package com.drewhiggins.bukkit.RandomChests;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
@@ -12,12 +11,16 @@ import java.util.Random;
 
 public class RandomChests extends JavaPlugin {
 
-    private final World world = Bukkit.getWorld("world");
+    private final World world = getServer().getWorld("world");
+    private final int MAX_X = 25000; // range extrema for locations randomly generated
+    private final int MAX_Z = 25000;
+    private final int MIN_X = -25000;
+    private final int MIN_Z = -25000;
 
     @Override
     public void onEnable() {
         getLogger().info("Random Chests has been enabled!");
-        BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
+        BukkitScheduler scheduler = getServer().getScheduler();
         scheduler.scheduleSyncRepeatingTask(this, new Runnable() {
             @Override
             public void run() {
@@ -34,8 +37,8 @@ public class RandomChests extends JavaPlugin {
 
     public Location getRandomLocation() {
         Random random = new Random();
-        int randX = random.nextInt();
-        int randZ = random.nextInt();
+        int randX = random.nextInt(MAX_X - MIN_X) + MIN_X;
+        int randZ = random.nextInt(MAX_Z - MIN_Z) + MIN_Z;
         int randY = world.getHighestBlockYAt(randX, randZ);
         return new Location(world, randX, randY, randZ);
     }
@@ -47,7 +50,7 @@ public class RandomChests extends JavaPlugin {
     public void spawnChest(Location location) {
         Block block = location.getBlock();
         block.setType(Material.CHEST);
-        Bukkit.getServer().broadcastMessage("A new chest has spawned at " + locationToString(location));
+        getServer().broadcastMessage("A new chest has spawned at " + locationToString(location));
     }
 
 }
